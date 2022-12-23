5552bed23ea8e025e85b7293db71bb9b093032cf
Hithesh Sampathi <hithesh.sampathi@gmail.com>
2022-12-23 16:00:34 +0530

    property updates

--- src/test/java/com/hithesh/embeddedserver/EmbeddedServerApplicationTests.java
+++ src/test/java/com/hithesh/embeddedserver/EmbeddedServerApplicationTests.java
@@ -2,12 +2,18 @@ package com.hithesh.embeddedserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 
 @SpringBootTest
 class EmbeddedServerApplicationTests {
 
+	@MockBean
+	private ServletWebServerApplicationContext webServerAppCtxt;
+
 	@Test
 	void contextLoads() {
 	}
+
 
 }
